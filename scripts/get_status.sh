b8f6a5869128dd816d5cb4ff76aa7eb7d2374837
Andras Nemeth <cucu.szpem@gmail.com>
2020-01-22 00:03:14 +0100

    [scripts/get_status] monitoring added

--- scripts/get_status.sh
+++ scripts/get_status.sh
@@ -11,4 +11,14 @@ fi
 . "$PARENT_DIR/virtualenv/bin/activate"
 
 cd "$PARENT_DIR"
-python get_status.py
+
+if [[ "$1" == "cloudwatch" ]]; then
+  STATUS=$(python get_status.py)
+  SUCCESS=$(echo "${STATUS}" | cut -d' ' -f2)
+  RESPONSE_TIME=$(echo "${STATUS}" | cut -d' ' -f3)
+  /usr/bin/aws cloudwatch put-metric-data --namespace KTStatus --metric-name Status --value "${SUCCESS}"
+  /usr/bin/aws cloudwatch put-metric-data --namespace KTStatus --metric-name ResponseTime --value "${RESPONSE_TIME}" --unit Seconds
+  echo "${STATUS}"
+else
+  python get_status.py
+fi
